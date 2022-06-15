41c00f1279af2e96ab8da545b02f76cea7718be4
80LK <80lkru@gmail.com>
2022-06-15 22:09:57 +0300

    add options for dependences

--- README.md
+++ README.md
@@ -7,12 +7,39 @@ Write in your launch-file
 IMPORT("CheckDeps");
 
 new CheckDeps()
-	//The entire API will be available under the API name
+	//The entire API will be available under the API name.
 	.add("name_api") 
-	// Add a dependency with a custom callback. The return value of the callback will be available under the API name
+	// Add a dependency with a custom callback. The return value of the callback will be available under the API name.
 	.add("name_api_2", function (api) {
 		return api.something;
 	}) 
+	// Add a dependency with options.
+	.add("name_api_3", {
+		/**
+		 * Required dependency. If this API has not been loaded, the mod will not run.
+		 * Optional parameter.
+		 * Default: true
+		 */
+		required: false,
+		/**
+		 * Callback on successful loading of the API. The return value of the callback will be available under the API name.
+		 * Optional parameter.
+		 * Default: function (api) { return api }
+		 */
+		callback: function (api) {
+			return api;
+		},
+		/**
+		 * Callback if the API has not been loaded. The return value of the callback will be available under the API name.
+		 * Optional parameter.
+		 */
+		fallback: function(){
+			return {
+				/* My scope */
+				a:12
+			}
+		}
+	})
 	//Launch mod with builded scope
 	.launch(function(scope){ Launch(scope); });
 ```
--- src/index.ts
+++ src/index.ts
@@ -14,41 +14,111 @@ LIBRARY({
 	shared: false
 });
 
+type Dict<T = any> = {
+	[key: string]: T;
+};
+
+function assignObject<S extends {}, T extends {}>(src: S, obj: T): S & T {
+	throw "";
+}
+
+interface DepsCallback {
+	(api: any): any;
+}
+interface DepsFallback {
+	(): any;
+}
+interface DepsOptions {
+	required?: boolean;
+	callback?: DepsCallback;
+	fallback?: DepsFallback;
+}
+
+interface DepsInfo extends DepsOptions {
+	loaded: boolean;
+}
+type DepsInfoDict = Dict<DepsInfo>;
+
+interface LaunchScope {
+	(scope: any): void;
+}
+
+
 class CheckDeps {
-	private scope: { [key: string]: any } = {};
-	private deps: {
-		[key: string]: {
-			loaded: boolean;
-			callback: (api: any) => any;
-		}
-	} = {};
+	private scope: Dict = {};
+	private requiredDeps: DepsInfoDict = {};
 	private countDeps = 0;
 	private initedDeps = 0;
+
+	private additiveDeps: DepsInfoDict = {};
 	private laucnhing = false;
 
-	public add(apiName: string, callback: (api: any) => any = (api) => api) {
+	public add(apiName: string): this;
+	public add(apiName: string, callback: DepsCallback): this;
+	public add(apiName: string, options: DepsOptions): this;
+	public add(apiName: string, options: DepsCallback | DepsOptions = (api) => api): this {
 		if (this.laucnhing) {
 			return this;
 		}
-		this.countDeps++;
-		this.deps[apiName] = { callback, loaded: false };
+
+		if (typeof options == "function")
+			options = { callback: options };
+
+		if (options.callback === undefined) options.callback = (api) => api;
+
+		if (options.required === undefined || options.required === true) {
+			this.countDeps++;
+			this.requiredDeps[apiName] = { loaded: false, callback: options.callback };
+		} else {
+			this.additiveDeps[apiName] = { loaded: false, callback: options.callback, fallback: options.fallback }
+		}
+
 		return this;
 	}
 
-	public launch(callback: (scope: any) => void) {
+	public launch(callback: LaunchScope): void {
 		if (this.laucnhing) return;
 		this.laucnhing = true;
-		for (const depsName in this.deps) {
-			const deps = this.deps[depsName];
+
+		for (const depsName in this.requiredDeps) {
+			const deps = this.requiredDeps[depsName];
 			ModAPI.addAPICallback(depsName, (api: any) => {
 				this.scope[depsName] = deps.callback(api);
 				deps.loaded = true;
-				this.initedDeps++;
 
-				if (this.initedDeps == this.countDeps)
-					callback(this.scope)
-			})
+				this.initedDeps++;
+			});
 		}
+
+		for (const depsName in this.requiredDeps) {
+			const deps = this.requiredDeps[depsName];
+			ModAPI.addAPICallback(depsName, (api: any) => {
+				this.scope[depsName] = deps.callback(api);
+				deps.loaded = true;
+
+				this.initedDeps++;
+			});
+		}
+
+		for (const depsName in this.additiveDeps) {
+			const deps = this.additiveDeps[depsName];
+			ModAPI.addAPICallback(depsName, (api: any) => {
+				this.scope[depsName] = deps.callback(api);
+				deps.loaded = true;
+			});
+		}
+
+		Callback.addCallback("ModsLoaded", () => {
+			if (this.initedDeps == this.countDeps) {
+				for (const depsName in this.additiveDeps) {
+					const deps = this.additiveDeps[depsName];
+					if (!deps.loaded && deps.fallback) {
+						this.scope[depsName] = deps.fallback();
+					}
+				}
+				callback(this.scope)
+			}
+		});
 	}
 }
 
